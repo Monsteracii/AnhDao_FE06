2ae42d559cd4bccadd88470a515dabb09a6c21c2
Dao Dao <anhdaonguyen.2611@gmail.com>
2024-10-13 23:12:53 +0900

    upload done hw

--- 20241008 - Array/BTVN-Array/Bai24.java
+++ 20241008 - Array/BTVN-Array/Bai24.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-//Nhập số n và dãy các số nguyên a[0], a[1],..., a[n-1] rồi sắp xếp dãy trên theo thứ tự tăng dần.
-public class Bai3 {
+//Thực hiện nhập vào một mảng số nguyên a có n phần tử (n > 0). 
+//Thực hiện sắp xếp mảng theo thứ tự giảm dần.
+public class Bai24 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
         System.out.print("Please input a number: ");
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
+        int n = scanner.nextInt(); 
+        int arr[] = new int[n]; 
         for (int i = 0; i < n; i++) {
             System.out.print("Enter the " + i + "-th element: ");
             arr[i] = scanner.nextInt();
@@ -13,17 +14,17 @@ public class Bai3 {
 
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i]; 
                     arr[i] = arr[j]; 
                     arr[j] = temp; 
                 }
             }
         }
 
-        System.out.println("The following series of numbers after arranging in ascending order: ");
+        System.out.println("The following series of numbers after arranging in descending order: ");
         for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.println(arr[i] + " ");
         }
         System.out.println();
     }
---
+++ 20241008 - Array/BTVN-Array/Bai25.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Bai25 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Please input a number: ");
+        int n = scanner.nextInt(); 
+        int arr[] = new int[n]; 
+        int countOdd = 0;
+        int countEven = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter the " + i + "-th element: ");
+            arr[i] = scanner.nextInt();
+        }
+
+        /* for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) countEven++;
+                else countOdd++;
+            } */
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) countEven++;
+            if (i % 2 != 0) countOdd++;
+        }
+        
+        System.out.println("Even numbers: " + countEven);
+        System.out.println("Odd numbers: " + countOdd);
+    }     
+}
